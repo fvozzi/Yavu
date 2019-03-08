fe3ccae95091ccb4237fb415b47df0b351d15a1c
fvozzi <facundov79@gmail.com>
2019-03-07 22:47:46 -0300

    Travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,9 +12,12 @@ deploy-simple() {
 	mkdir "$ARTIFACT_DIR"
 	cp "$SMALLTALK_CI_IMAGE" "${ARTIFACT_DIR}/${PROJECT_NAME}.image"
 	cp "$SMALLTALK_CI_CHANGES" "${ARTIFACT_DIR}/${PROJECT_NAME}.changes"
-	cp -r "${TRAVIS_BUILD_DIR}/pharo/img" "${ARTIFACT_DIR}/img"
-	cp -r "${SMALLTALK_CI_HOME}/pharo/img" "${ARTIFACT_DIR}/img"
-	cp -r "${SMALLTALK_CI_BUILD}/pharo/img" "${ARTIFACT_DIR}/img"
+	ls "${TRAVIS_BUILD_DIR}"
+	ls "${SMALLTALK_CI_HOME}"
+	ls "${SMALLTALK_CI_BUILD}"
+	#cp -r "${TRAVIS_BUILD_DIR}/pharo/img" "${ARTIFACT_DIR}/img"
+	#cp -r "${SMALLTALK_CI_HOME}/pharo/img" "${ARTIFACT_DIR}/img"
+	#cp -r "${SMALLTALK_CI_BUILD}/pharo/img" "${ARTIFACT_DIR}/img"
 	
 	
 	
