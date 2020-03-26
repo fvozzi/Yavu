3c147ed12ad1d463d210ee1e8789d124cc91bd59
fvozzi <facundov79@gmail.com>
2020-03-26 17:50:25 -0300

    Change project name to yavu

--- scripts/post-process.sh
+++ scripts/post-process.sh
@@ -3,7 +3,7 @@
 # wherever you'll be ssh-ing into user@machine
 readonly TARGET_MACHINE="root@192.241.172.175"
 # customize the name of the Pharo image you will be deploying
-readonly PROJECT_NAME="Yavu"
+readonly PROJECT_NAME="yavu"
 # customize the name of the Pharo image you will be deploying
 readonly TRAVIS_BUILD_NUMBER="$1"
 # customize the name of the build folder
